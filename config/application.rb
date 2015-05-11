e5835680713227ce6d4a5e78137fd3c4a2bdea46
Kamil Nogala <kamil.nogala@gmail.com>
2015-05-11 16:22:26 +0200

    application name change - this isnt important for tutorial participant

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Getwolfpack2
+module Getwolfpack
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
